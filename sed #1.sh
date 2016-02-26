eae7498594d16de7fcb8b630e8d9d899fedd4b61
Sergio Gonzalez Collado <sergio.collado@gmail.com>
2016-02-26 20:10:03 +0100

    sed command

---
+++ sed #1.sh
@@ -0,0 +1,4 @@
+#Task 
+#For each line in a given input file, transform the first occurrence of the word 'the' with 'this'. The search and transformation #should be strictly case sensitive.
+
+sed -s 's/ the / this /'
