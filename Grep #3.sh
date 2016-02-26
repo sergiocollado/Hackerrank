13f656209b0c38ce17b645be2188c9b8861ccacc
Sergio Gonzalez Collado <sergio.collado@gmail.com>
2016-02-26 19:41:12 +0100

    Grep#3

---
+++ Grep #3.sh
@@ -0,0 +1,4 @@
+#Task 
+#You are given a text file that will be piped into your command through STDIN. Use grep to remove all those lines that contain the #word 'that'. The search should NOT be sensitive to case.
+
+grep -iwv "that"
