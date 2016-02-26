eba867e964bdcc1a5322658c5a52a9a535e8dc72
Sergio Gonzalez Collado <sergio.collado@gmail.com>
2016-02-26 19:35:46 +0100

    Grep#1

---
+++ Grep #1.sh
@@ -0,0 +1,5 @@
+#https://youtu.be/WX5Zfflvdt0
+
+#Task 
+#You are given a text file that will be piped into your command through STDIN. Use grep to display all the lines that contain the #word the in them. The search should be sensitive to case. Display only those lines of the input file that contain the word 'the'.
+grep -w "the"
